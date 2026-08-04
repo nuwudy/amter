e1457c21b4638b1505a9bc15ec76582ada934ee6
nuwudy <nuwudy@gmail.com>
2026-08-04 21:10:31 +0530

    Move See Malayalam toggle below English text in Listen and Speak block

--- resources/views/filament/components/unit-renderer-student.blade.php
+++ resources/views/filament/components/unit-renderer-student.blade.php
@@ -937,18 +937,7 @@
                                                 </button>
                                             @endif
                                             
-                                            <!-- See Malayalam Toggle -->
-                                            @if(!empty($meaningMalayalam))
-                                                <button type="button" 
-                                                        @click="showMalayalam = !showMalayalam"
-                                                        class="transcript-toggle-btn btn-malayalam-toggle"
-                                                        :class="{ 'is-active': showMalayalam }">
-                                                    <!-- Icon -->
-                                                    <svg x-show="!showMalayalam" style="width: 14px; height: 14px;" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="3"><path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.644C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"/><path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/></svg>
-                                                    <svg x-show="showMalayalam" style="width: 14px; height: 14px;" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="3" x-cloak><path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.893 7.893L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" /></svg>
-                                                    <span x-text="showMalayalam ? 'Hide Malayalam' : 'See Malayalam'">See Malayalam</span>
-                                                </button>
-                                            @endif
+                                            <!-- See Malayalam Toggle moved below English text -->
                                         </div>
                                         
                                         <!-- English Text Area -->
@@ -961,6 +950,21 @@
                                                 "{{ $englishText }}"
                                             </h3>
                                         </div>
+                                        
+                                        <!-- See Malayalam Toggle -->
+                                        @if(!empty($meaningMalayalam))
+                                            <div style="width: 100%; display: flex; justify-content: center; margin-top: 0.25rem;">
+                                                <button type="button" 
+                                                        @click="showMalayalam = !showMalayalam"
+                                                        class="transcript-toggle-btn btn-malayalam-toggle"
+                                                        :class="{ 'is-active': showMalayalam }">
+                                                    <!-- Icon -->
+                                                    <svg x-show="!showMalayalam" style="width: 14px; height: 14px;" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="3"><path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.644C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"/><path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/></svg>
+                                                    <svg x-show="showMalayalam" style="width: 14px; height: 14px;" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="3" x-cloak><path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.893 7.893L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" /></svg>
+                                                    <span x-text="showMalayalam ? 'Hide Malayalam' : 'See Malayalam'">See Malayalam</span>
+                                                </button>
+                                            </div>
+                                        @endif
                                         
                                         <!-- Malayalam Meaning Area -->
                                         @if(!empty($meaningMalayalam))
